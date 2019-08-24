78e43e0ef2cb7814b03ce855597809581e975693
Graham Kerr <kerr.graham.d@gmail.com>
2019-08-24 15:39:42 -0600

    Added all project files
    primary commit

---
+++ ProducerConsumerTest/Connection.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace ProducerConsumerTest
+{
+    class Connection
+    {
+        private BlockingCollection<AsyncResponse> queue = new BlockingCollection<AsyncResponse>();
+        private CancellationToken cancelToken = new CancellationToken();
+        private Func<Command, Response> _func = null;
+
+        public bool Running { get; private set; }
+      
+        public Connection()
+        {
+            Task.Factory.StartNew(() => DoWork(cancelToken), TaskCreationOptions.LongRunning);
+        }
+        public void Dispose()
+        {
+            Running = false;
+
+            while (queue.Count > 0) //loop until queue is empty
+            {
+                var data = queue.Take();
+                data.Dispose();
+            }
+        }
+
+        public void SetFunc(Func<Command, Response> func)
+        {
+            _func = func;
+        }
+
+        public Response SendCommand(Command cmd)
+        {
+            var Reply = new AsyncResponse(cmd);
+            queue.Add(Reply);
+
+            return Reply.GetReturn();
+        }
+        private void DoWork(CancellationToken token)
+        {
+           try
+           {
+                Running = true;
+
+                while(Running == true)
+                {
+                    var cmd = queue.Take(token);
+
+                    //----- do work here ----
+                    //-----------------------
+
+                    Response reply = _func(cmd.cmd);
+                    cmd.SetResponse(reply);
+                }
+           }
+            catch(Exception e)
+           {
+               this.Dispose();
+           }        
+        }
+
+
+        private class AsyncResponse
+        {
+            private Response _returnData = null;
+            private ManualResetEvent _waithandle = new ManualResetEvent(false);
+
+            public Command cmd { get; private set; }
+
+            public AsyncResponse(Command cmd)
+            {
+                _waithandle.Reset();
+                this.cmd = cmd;
+            }
+            public void SetResponse(Response Reply)
+            {
+                _returnData = Reply;
+                _waithandle.Set();
+            }
+
+            public void Dispose()
+            {
+                _waithandle.Set();
+            }
+
+            public Response GetReturn()
+            {
+                _waithandle.WaitOne();
+                return _returnData;
+            }
+        }
+
+
+    }
+
+    class Command
+    {
+        private void test()
+        {
+            Connection con = new Connection();
+            con.SetFunc(DoWork);
+
+        }
+
+        private Response DoWork(Command cmd)
+        {
+            return new Response();
+        }
+    }
+
+    class Response
+    {
+
+    }
+
+
+
+
+}
---
+++ ProducerConsumerTest/ThreadSafeFunction.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ProducerConsumerTest
+{
+    class ThreadSafeFunction<Arg, Ret>
+    {       
+        private BlockingCollection<AsyncResponse<Arg, Ret>> queue = new BlockingCollection<AsyncResponse<Arg, Ret>>();
+        private Func<Arg, Ret> _func = null;
+
+        private CancellationTokenSource _cancelToken = new CancellationTokenSource();
+ 
+        public bool Running { get; private set; }
+
+        public ThreadSafeFunction()
+        {
+            
+        }
+
+        public void Initialize(Func<Arg, Ret> func)
+        {
+            _func = func;
+            Task.Factory.StartNew(() => DoWork(_cancelToken.Token), TaskCreationOptions.LongRunning);
+        }
+
+        public void Dispose()
+        {
+            Running = false;
+            _cancelToken.Cancel();
+
+            while (queue.Count > 0) //loop until queue is empty
+            {
+                var data = queue.Take();
+                data.Dispose();
+            }
+        }
+
+        public Ret Run(Arg Parameter)
+        {
+            var data = new AsyncResponse<Arg, Ret>(Parameter);
+            queue.Add(data);
+
+            return data.GetReturn();
+        }
+        private void DoWork(CancellationToken token)
+        {
+            try
+            {
+                Running = true;
+
+                while (Running == true)
+                {
+                    var data = queue.Take(token);
+
+                    Ret reply = _func(data.input);
+                    data.SetReturnValue(reply);
+                }
+            }
+            catch (Exception e)
+            {
+                this.Dispose();
+            }
+        }
+
+
+        private class AsyncResponse<Arg, Ret>
+        {
+            private Ret _returnData;
+            private ManualResetEvent _waithandle = new ManualResetEvent(false);
+
+            public Arg input { get; private set; }
+
+            public AsyncResponse(Arg input)
+            {
+                _waithandle.Reset();
+                this.input = input;
+            }
+            public void Dispose()
+            {
+                _waithandle.Set();
+            }
+
+            public void SetReturnValue(Ret ReturnValue)
+            {
+                _returnData = ReturnValue;
+                _waithandle.Set();
+            }
+            public Ret GetReturn()
+            {
+                _waithandle.WaitOne();
+                return _returnData;
+            }
+        }
+
+    }
+
+
+
+    public class Tester
+    {
+        private ThreadSafeFunction<int[], int> TSFunction = new ThreadSafeFunction<int[],int>();
+
+        public Tester()
+        {
+            
+        }
+        public int calculate()
+        {
+            TSFunction.Initialize(Calculate);
+
+            Task.Factory.StartNew(() => doWork(), TaskCreationOptions.LongRunning);
+
+            var val1 = TSFunction.Run(new int[] { 1, 1, 2, 3, 4 });
+            var val2 = TSFunction.Run(new int[] { 1, 1, 2, 3, 4 });
+
+            TSFunction.Dispose();
+
+            return val2;
+        }
+        public void doWork()
+        {
+            var val1 = TSFunction.Run(new int[] { 1, 1, 2, 3, 4 });
+            var val2 = TSFunction.Run(new int[] { 1, 1, 2, 3, 4 });
+        }
+
+
+        private int Calculate(int[] input)
+        {
+            int sum = 0;
+
+            foreach (int number in input)
+            {
+                sum += number;
+                Thread.Sleep(200);
+            }
+
+            return sum;
+        }
+    }
+
+}
